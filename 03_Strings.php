96c0355b1ce5d29645cbc2a638bee24c49c382e2
Omkar Abhishek Jha <jhaomkar396@gmail.com>
2024-05-25 21:26:36 +0530

    sixth

--- 03_Strings.php
+++ 03_Strings.php
@@ -24,7 +24,32 @@
 <body>
     <div class="container">
         <?php
-            echo "HELLO";
+            echo "HELLO <br>";
+
+            //String Manipulation
+            $str = "This";
+            echo $str;
+
+            //Length of the string
+            $length = strlen($str);
+            echo "<br> The length of the string is : $length";
+
+            //Concatenation
+
+            $str1 = "I am";
+            $str2 = " a Student";
+
+            echo "<br>";
+            echo "The concatenated string is : ";
+            echo $str1.$str2;
+
+            echo "<br> The number of words in the first string is : ". str_word_count($str);
+
+            $str3 = "This a string ";
+            echo "<br> The original string is : $str3";
+            echo "<br> The reversed string is :". strrev($str3); 
+
+            echo "<br> The position of 'a' in the third string is : ". strpos($str3,"a");
         ?>
     </div>
 </body>
