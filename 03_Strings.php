11b7eb467f32517d70611b7ddaad4a23fdc4f8f4
Omkar Abhishek Jha <jhaomkar396@gmail.com>
2024-05-25 21:05:57 +0530

    fifth

---
+++ 03_Strings.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<style>
+    * {
+        margin: 5px;
+        padding: 5px;
+        box-sizing: border-box;
+    }
+
+    .container {
+        max-width: 60%;
+        background-color: rosybrown;
+        margin: auto;
+        padding: 25px;
+    }
+</style>
+
+<body>
+    <div class="container">
+        <?php
+            echo "HELLO";
+        ?>
+    </div>
+</body>
+
+</html>
